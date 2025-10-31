e336c63c773f2bbb2210508db8e3dfa5bd966350
Any-key-Anton <edlinanykey@gmail.com>
2025-10-31 12:34:41 +0300

    Update ph_sensor.h

--- components/ph_sensor/ph_sensor.h
+++ components/ph_sensor/ph_sensor.h
@@ -3,14 +3,12 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
-#include "iarduino_I2C_pH.h"
+// #include "iarduino_I2C_pH.h" // Включите, если файл лежит внутри папки компонента
+#include "iarduino_I2C_pH.h" // Или так, если файл доступен в пути сборки
 
-// Пространство имён: ph_sensor (без esphome::)
-namespace ph_sensor {
+namespace ph_sensor { // <-- ВАЖНО: Это пространство имён
 
-class PHSensor : public esphome::PollingComponent,
-                 public esphome::sensor::Sensor,
-                 public esphome::i2c::I2CDevice {
+class PHSensor : public esphome::PollingComponent, public esphome::sensor::Sensor, public esphome::i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
