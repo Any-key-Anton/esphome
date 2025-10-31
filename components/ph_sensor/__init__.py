f8187c72ac2eb2c30b3b938bdf1f1901f46bd03e
Any-key-Anton <edlinanykey@gmail.com>
2025-10-31 11:55:50 +0300

    Update __init__.py

--- components/ph_sensor/__init__.py
+++ components/ph_sensor/__init__.py
@@ -17,8 +17,8 @@ AUTO_LOAD = ["sensor"]
 DEPENDENCIES = ["i2c"]
 
 # Создаём пространство имён для нашего компонента
-# Убедись, что в ph_sensor.h используется namespace ph_sensor или esphome::ph_sensor
-ph_sensor_ns = cg.esphome_ns.namespace("ph_sensor")
+# ИСПРАВЛЕНО: теперь соответствует namespace esphome { namespace ph_sensor { ... }}
+ph_sensor_ns = cg.esphome_ns.namespace("esphome::ph_sensor")
 
 # Определяем C++ класс
 # Наследуемся от PollingComponent (для update_interval), Sensor и I2CDevice
@@ -27,29 +27,21 @@ PHSensor = ph_sensor_ns.class_(
 )
 
 # Определяем схему конфигурации
-CONFIG_SCHEMA = sensor.sensor_schema( # Используем sensor.schema для создания сенсора
-    PHSensor,                         # Указываем наш C++ класс
-    unit_of_measurement=UNIT_PH,      # Единицы измерения
-    accuracy_decimals=3,              # Знаки после запятой
-    device_class=DEVICE_CLASS_PH,     # Класс устройства
-    state_class=STATE_CLASS_MEASUREMENT, # Класс состояния
-    icon="mdi:ph"                     # Иконка
-).extend(                             # Добавляем общие настройки компонента
-    cv.polling_component_schema("60s") # Настройки PollingComponent (update_interval)
-).extend(                           # Добавляем настройки I2C
-    i2c.i2c_device_schema(0x00)      # Схема I2C устройства, 0x00 по умолчанию
+CONFIG_SCHEMA = sensor.sensor_schema(
+    PHSensor,
+    unit_of_measurement=UNIT_PH,
+    accuracy_decimals=3,
+    device_class=DEVICE_CLASS_PH,
+    state_class=STATE_CLASS_MEASUREMENT,
+    icon="mdi:ph"
+).extend(
+    cv.polling_component_schema("60s")
+).extend(
+    i2c.i2c_device_schema(0x00)
 )
 
-# Функция, генерирующая C++ код
 async def to_code(config):
-    # Создаём новую переменную C++ класса PHSensor
     var = cg.new_Pvariable(config[CONF_ID])
-
-    # Регистрируем компонент (для setup, loop и т.д.)
     await cg.register_component(var, config)
-
-    # Регистрируем его как sensor
     await sensor.register_sensor(var, config)
-
-    # Регистрируем его как I2CDevice
     await i2c.register_i2c_device(var, config)
