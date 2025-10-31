698c8bfbde0630fa9282077d0fdbd5dcdc461954
Any-key-Anton <edlinanykey@gmail.com>
2025-10-31 11:27:38 +0300

    Update ph_sensor.cpp

--- components/ph_sensor/ph_sensor.cpp
+++ components/ph_sensor/ph_sensor.cpp
@@ -6,33 +6,37 @@ namespace ph_sensor {
 
 static const char *const TAG = "ph_sensor";
 
-void PH_Sensor::setup() {
+void PHSensor::setup() {
   ESP_LOGCONFIG(TAG, "Setting up pH Sensor...");
-  if (!this->ph_.begin(&this->parent_->get_wire())) {
+  // Передаём указатель на Wire-объект из I2CDevice
+  if (!this->ph_.begin(&this->get_wire())) {
     ESP_LOGE(TAG, "Failed to initialize pH sensor!");
     this->mark_failed();
     return;
   }
-  if (this->address_ != 0) {
+  // Если адрес не 0x00, меняем его
+  if (this->address_ != 0x00) {
     this->ph_.changeAddress(this->address_);
   }
 }
 
-void PH_Sensor::dump_config() {
+void PHSensor::dump_config() {
   ESP_LOGCONFIG(TAG, "pH Sensor:");
   LOG_I2C_DEVICE(this);
+  LOG_SENSOR("  ", "pH", this); // Добавляем логирование сенсора
   LOG_UPDATE_INTERVAL(this);
 }
 
-void PH_Sensor::update() {
+void PHSensor::update() {
   float ph_value = this->ph_.getPH();
   if (ph_value > 0 && ph_value <= 14.0f) {
-    if (this->sensor_ != nullptr) {
-      this->sensor_->publish_state(ph_value);
-    }
+    // Публикуем состояние напрямую через this->publish_state()
+    this->publish_state(ph_value);
     ESP_LOGD(TAG, "Got pH: %.3f", ph_value);
   } else {
     ESP_LOGW(TAG, "Invalid pH reading: %.3f", ph_value);
+    // Можно опубликовать NAN, чтобы показать недостоверное значение
+    // this->publish_state(NAN);
   }
 }
 
